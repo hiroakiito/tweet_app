77940078f5c4a9262622a7f1aadccfd668f579eb
Hiroaki Ito <pigaru.gooood.mar19@gmail.com>
2018-05-25 05:10:57 +0000

    9.20

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,22 @@
 class SessionsController < ApplicationController
+
   def new
   end
-  
-  def create#ユーザーをデータベースから見つけて検証する
-    user = User.find_by(email:params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])#「ユーザーがデータベースにあり、かつ、認証に成功した場合にのみ」
-     log_in user
-     redirect_to user
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      remember user
+      redirect_to user
     else
-       flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
-    render 'new'
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
   end
-  
+
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
-  
 end
