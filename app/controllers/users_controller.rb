93a024d816be6c606f7d7f8d534eaa55a8a2439f
Hiroaki Ito <pigaru.gooood.mar19@gmail.com>
2018-05-25 05:34:25 +0000

    10.8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,22 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      # 保存の成功をここで扱う。
-      redirect_to @user#redirect_to user_url(@user)と等価
+      redirect_to @user
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+    else
+      render 'edit'
     end
   end
 
